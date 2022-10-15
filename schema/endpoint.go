375338d313ac27c473ae4fdc1f87b642c5398e1c
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-10-15 19:48:42 +0330

    New: Added the new schema

--- schema/endpoint.go
+++ schema/endpoint.go
@@ -7,12 +7,13 @@ import (
 
 type Endpoint struct {
 	gorm.Model
-	Path        string         `gorm:"unique;not null" json:"path"`
-	Tier        int            `gorm:"not null" json:"tier"`
-	Assets      datatypes.JSON `gorm:"not null" json:"assets"`
-	Currencies  datatypes.JSON `gorm:"not null" json:"currencies"`
-	Resolutions datatypes.JSON `gorm:"not null" json:"resolutions"`
-	Formats     datatypes.JSON `gorm:"not null" json:"formats"`
+	Path         string         `gorm:"unique;not null" json:"path"`
+	Tier         int            `gorm:"not null" json:"tier"`
+	Assets       datatypes.JSON `gorm:"not null" json:"assets"`
+	Currencies   datatypes.JSON `gorm:"not null" json:"currencies"`
+	Resolutions  datatypes.JSON `gorm:"not null" json:"resolutions"`
+	Formats      datatypes.JSON `gorm:"not null" json:"formats"`
+	ChildGroupID uint
 }
 
 type Asset struct {
---
+++ schema/group.go
@@ -0,0 +1,23 @@
+package schema
+
+import "gorm.io/gorm"
+
+type TopGroup struct {
+	gorm.Model
+	Name         string `gorm:"not null;unique"`
+	MiddleGroups []MiddleGroup
+}
+
+type MiddleGroup struct {
+	gorm.Model
+	Name        string `gorm:"not null;unique"`
+	TopGroupID  uint
+	ChildGroups []ChildGroup
+}
+
+type ChildGroup struct {
+	gorm.Model
+	Name          string `gorm:"not null;unique"`
+	MiddleGroupID uint
+	Endpoints     Endpoint
+}
