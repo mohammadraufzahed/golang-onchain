c493e9a5f905ce4bfb65ef4fda0aa1c40847fd79
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-10-28 19:17:49 +0330

    Update: added the config to influx

--- influxdb/connect.go
+++ influxdb/connect.go
@@ -1,11 +1,12 @@
 package influxdb
 
 import (
+	"github.com/ario-team/glassnode-api/config"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 var Client influxdb2.Client
 
 func Connect() {
-	Client = influxdb2.NewClient("http://localhost:8086", "9U9sKKDHo5JYW6ZoXCch7dqWk6bZNhVb0O-n7CP13c0NQMrO6sHt_QpUUIYDryjlQNBEcYz6k9dqbtELgmCo_Q==")
+	Client = influxdb2.NewClient(config.Viper.GetString("INFLUX_URL"), config.Viper.GetString("INFLUX_TOKEN"))
 }
