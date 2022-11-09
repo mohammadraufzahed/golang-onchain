8d1211c8566c37b2d6f42f6f6361bd315e5370c3
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-11-09 01:41:32 -0800

    New: Added the chart reciver

--- functions/charts.go
+++ functions/charts.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/ario-team/glassnode-api/config"
 	"github.com/ario-team/glassnode-api/database"
 	"github.com/ario-team/glassnode-api/influxdb"
+	"github.com/ario-team/glassnode-api/redis"
 	"github.com/ario-team/glassnode-api/schema"
 	"github.com/ario-team/glassnode-api/types"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
@@ -75,4 +77,39 @@ func InitializeChart(endpointID uint) {
 	}
 	endpoint.Initialized = true
 	database.Connection.Save(&endpoint)
+}
+
+type chartReturn struct {
+	T int64 `json:"time"`
+	V any   `json:"value"`
+}
+
+func GetChart(id int, asset string, interval string, start uint, end uint) (chart string, err error) {
+	var endpoint schema.Endpoint
+	database.Connection.Where("id = ?", id).First(&endpoint)
+	redisKey := fmt.Sprintf("%v/%v/%v/%v/%v", endpoint.Path, asset, interval, start, end)
+	exists := redis.Exists(redisKey)
+	var chartJson string
+	if exists == 1 {
+		return redis.Get(redisKey), nil
+	} else {
+		queryApi := influxdb.Client.QueryAPI("glassnode")
+		result, err := queryApi.Query(context.Background(), fmt.Sprintf(`from (bucket: "glassnode") |> range(start: %v, stop: %v) |> filter(fn: (r) => r.asset == "%v" and r.resolution == "%v")`, start, end, strings.ToUpper(asset), interval))
+		if err != nil {
+			fmt.Println("Error")
+			return "", err
+		}
+		var chart []chartReturn
+		for result.Next() {
+			chart = append(chart, chartReturn{
+				T: result.Record().Time().Unix(),
+				V: result.Record().Value(),
+			})
+		}
+		chartJson, err := json.Marshal(chart)
+		if err == nil {
+			redis.Set(redisKey, chartJson, time.Hour)
+		}
+	}
+	return chartJson, err
 }
