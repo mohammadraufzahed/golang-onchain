0a777aabdf5eceb459ec83d8a2521c4124b0e4df
Sadegh Ismael Nattaj <sadegh@devio.ir>
2022-11-21 09:43:13 +0330

    Checks and create logger dir

--- logger/init.go
+++ logger/init.go
@@ -3,11 +3,16 @@ package logger
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var Logger *log.Logger = log.Default()
 
 func Init() {
+	if err:= os.MkdirAll(filepath.Join(".", "logs"), os.ModePerm); err != nil {
+	    log.Fatal(err)
+	}
+
 	// If the file doesn't exist, create it or append to the file
 	file, err := os.OpenFile("./logs/logs", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
